cc4f2f1e7737b18e564e45fc0fcaa5d11cdbd508
Nicolas Aimar <Nicoaimar@MacBook-Pro.fibertel.com.ar>
2022-10-11 17:52:16 -0300

    First Commit - Data Type Added

---
+++ Aimar-Entrega-1/src/com/nicoCompany/TiposDatos.java
@@ -0,0 +1,21 @@
+package com.nicoCompany;
+
+public class TiposDatos {
+    public static void main(String[] args) {
+        int numero = 54;
+        long numeroLargo = 54768468l;
+        double conDecimal = 54.56d;
+        boolean estoEsTrue = true;
+        String palabra = "This is a word";
+
+        System.out.println(numero);
+        System.out.println(numeroLargo);
+        System.out.println(conDecimal);
+        System.out.println(estoEsTrue);
+        System.out.println(palabra);
+    }
+}
+
+
+
+
